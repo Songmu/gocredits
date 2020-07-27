877aa000697039a9e37df9107c7383b7803a43ca
k1LoW <k1lowxb@gmail.com>
2020-07-28 01:24:41 +0900

    Add test

--- gocredits_test.go
+++ gocredits_test.go
@@ -1,6 +1,12 @@
 package gocredits
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
 
 func TestLicenseDirs_set(t *testing.T) {
 	ld := &licenseDirs{}
@@ -20,4 +26,32 @@ func TestLicenseDirs_set(t *testing.T) {
 	if len(ld.dirs["test"]) != 2 {
 		t.Errorf("len(ld.dirs[test]) should be 2 but: %d", len(ld.dirs["test"]))
 	}
+}
+
+func TestTakeCredits(t *testing.T) {
+	tests := []struct {
+		name          string
+		dir           string
+		skipNoLicense bool
+		wantErr       error
+	}{
+		{"go.sub only", "gosum_only", false, nil},
+		{"go.mod only", "gomod_only", false, nil},
+		{"there is neither go.mod nor go.sum", "no_gomod_no_gosum", false, fmt.Errorf("use go modules")},
+		{"no licenses package found", "no_license", false, fmt.Errorf("no licenses found for \"github.com/Songmu/no_license_pkg\"")},
+		{"no licenses package found. but skip", "no_license", true, nil},
+	}
+	for _, tt := range tests {
+		dir := filepath.Join(testdataDir(), tt.dir)
+		_, gotErr := takeCredits(dir, tt.skipNoLicense)
+		if !reflect.DeepEqual(gotErr, tt.wantErr) {
+			t.Errorf("%s:\ngot  %v\nwant %v", tt.name, gotErr, tt.wantErr)
+		}
+	}
+}
+
+func testdataDir() string {
+	wd, _ := os.Getwd()
+	dir, _ := filepath.Abs(filepath.Join(wd, "testdata"))
+	return dir
 }
